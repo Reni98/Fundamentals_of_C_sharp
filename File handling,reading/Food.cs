0372239dbb9e8c7010e024587ac428a492c49c64
Reni98 <93825160+Reni98@users.noreply.github.com>
2023-09-03 20:10:53 +0200

    Add files via upload

---
+++ File handling,reading/Food.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fájbeolvasás
+{
+    internal class Food
+    {
+        public int week;
+        public string meal;
+        public string name;
+        public string price;
+        public string pieces;
+
+        public Food(string week, string meal, string name, string price, string pieces)
+        {
+            this.week = int.Parse(week);
+            this.meal = meal;
+            this.name = name;
+            this.price = price;
+            this.pieces = pieces;
+        }
+    }
+}
---
+++ File handling,reading/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fájbeolvasás
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Reading:
+            List<Food> food_list = new List<Food>();
+            string[] lines = File.ReadAllLines("menu.txt");
+            foreach (var item in lines)
+            {
+                string[] values = item.Split(';');
+                Food food_object = new Food(values[0], values[1], values[2], values[3], values[4]);
+                food_list.Add(food_object);
+            }
+
+            foreach (var item in food_list)
+            {
+                Console.WriteLine($"{item.week};{item.meal};{item.name};{item.price};{item.pieces}");
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
