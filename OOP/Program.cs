38702900a10030a504059f79d059534ab0dae8fc
Reni98 <93825160+Reni98@users.noreply.github.com>
2023-09-13 22:42:14 +0200

    Add files via upload

---
+++ OOP/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kutya
+{
+    class Kutya
+    {
+        private string nev;
+        private int kor;
+
+        public Kutya(string nev, int kor)
+        {
+            this.nev = nev;
+            this.kor = kor;
+        }
+
+        public string GetAdatok()
+        {
+            return $"Név: {nev}, Kor: {kor}";
+        }
+
+
+
+
+
+    }
+
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Kutya k = new Kutya("Zeusz", 2);
+            Console.WriteLine(k.GetAdatok());
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
